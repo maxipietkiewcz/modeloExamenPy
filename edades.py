ed6345c0494e25f1a90a3a09e26668c9dc5204f9
maxipietkiewicz <maximilianopietkiewicz04@gmail.com>
2024-05-02 23:19:41 -0300

    grafico agregado

--- edades.py
+++ edades.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("edades.csv") # Lee el archivo CSV
 df['age'] = pd.to_datetime(df['age'], format='%d/%m/%Y') # Convierte la columna 'age' a datetime
@@ -15,3 +15,11 @@ age_counts = df['age'].dt.year.value_counts().sort_index() # Cuenta el número 
 result_df = pd.DataFrame({'Edades': age_counts.index, 'Frecuencia': age_counts.values}) # Crea el DataFrame
 
 print(result_df) # Imprime el DataFrame
+
+
+plt.plot(result_df['Edades'], result_df['Frecuencia'], marker='o', linestyle='-') # Grafica la frecuencia de edades
+plt.title('Frecuencia de Edades') # Agrega un título
+plt.xlabel('Edades') # Agrega una etiqueta al eje x
+plt.ylabel('Frecuencia') # Agrega una etiqueta al eje y
+plt.grid(True) # Agrega una cuadrícula
+plt.show() # Muestra el gráfico
